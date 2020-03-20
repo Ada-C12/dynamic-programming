20a2a5dbf38f231159f2fa6ade32c068f73cb2f4
Mira <calopter@gmail.com>
2020-03-19 17:06:30 -0700

    largest subarray sum

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,18 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+  return 0 if nums == nil
+  return if nums.empty?
+
+  max_so_far = 0
+  max_ending_here = 0
+
+  nums.each do |num|
+    max_ending_here += num
+    max_ending_here = 0 if max_ending_here < 0
+    max_so_far = max_ending_here if max_ending_here > max_so_far
+  end
+
+  return max_so_far > 0 ? max_so_far : nums.max
 end

ad8ceb130aa1969e74253a506ba85bd266863177
Mira <calopter@gmail.com>
2020-03-19 17:16:16 -0700

    consolidate loops

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -5,14 +5,16 @@ def max_sub_array(nums)
   return 0 if nums == nil
   return if nums.empty?
 
+  max = nums.first
   max_so_far = 0
   max_ending_here = 0
 
   nums.each do |num|
+    max = num if num > max
     max_ending_here += num
     max_ending_here = 0 if max_ending_here < 0
     max_so_far = max_ending_here if max_ending_here > max_so_far
   end
 
-  return max_so_far > 0 ? max_so_far : nums.max
+  return max_so_far > 0 ? max_so_far : max
 end

e89cf7fe45f2699a4da7a1b509169cd853dc8596
stupendousC <CarolineWuKaplan@gmail.com>
2020-03-19 17:16:05 -0700

    max sub array done, but is brute force :-|

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,35 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def max_sub_array(nums)
+    ### Brute force
     return 0 if nums == nil
     
-    raise NotImplementedError, "Method not implemented yet!"
+    curr_best_sum = nums[0]
+    
+    length = nums.length
+    
+    i = 0
+    while i < length
+        curr_sum = nums[i] 
+        if curr_sum > curr_best_sum
+            curr_best_sum = curr_sum
+        end
+        
+        j = i+1
+        while j < length
+            curr_sum += nums[j]
+            
+            if curr_sum > curr_best_sum
+                curr_best_sum = curr_sum
+            end
+            
+            j += 1
+        end
+        
+        i += 1
+        
+    end
+    
+    return curr_best_sum
 end

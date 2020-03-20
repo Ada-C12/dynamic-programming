2830ab5ad69c8e029c173fe302b26815811115c7
Dianna Tingg <dtingg@gmail.com>
2020-03-19 18:27:00 -0700

    added max_subarray

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,22 @@
-
 # Time Complexity: ?
 # Space Complexity: ?
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+  return nil if nums == nil || nums.empty?
+
+  max_so_far = 0
+  max_ending_here = 0
+
+  nums.each do |num|
+    max_ending_here += num
+
+    if (max_ending_here < 0)
+      max_ending_here = 0
+    end
+
+    if (max_so_far < max_ending_here)
+      max_so_far = max_ending_here
+    end
+  end
+
+  return max_so_far    
 end

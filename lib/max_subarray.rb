5facb717a925af761837aa251304143e42acdb9c
Xinran Che <gracexinran@gmail.com>
2020-03-19 18:07:39 -0700

    Completed

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,17 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+    return nil if nums == []
+    max_sum = prev = nums[0]
+    i = 1
+    while i < nums.length
+      curr = nums[i]
+      curr = [prev + curr, curr].max
+      max_sum = [max_sum, curr].max 
+      prev = curr
+      i += 1
+    end
+    return max_sum
 end

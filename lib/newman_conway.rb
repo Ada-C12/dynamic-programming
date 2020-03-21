d798c7b110b25735a0297febcc15844cf8fa7fd2
Julia A Kingrey <juliakingreyworks@gmail.com>
2020-03-20 18:14:07 -0700

    Adding time/space complexity and tweaks to notes.

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,7 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n) because we need to perform our "what is the p?" steps n times
+# Space Complexity: also O(n) because we are adding an element to the sequence array for every n
 def newman_conway(num)
   raise ArgumentError, "Argument must be an integer greater than zero." if num <= 0 
 
@@ -16,13 +16,11 @@ end
 
 def p_helper(num, sequence)
 
-  # need to find p of all whole integers preceding num and add them to sequence
-
   # BASE CASE: stop when count of numbers in sequence array is the same as `num`
   return sequence.join(" ") if num == sequence.length
 
-  # REGRESSIVE CASE: 
-  # forumula is p(num) = p(p(num - 1)) + p(num - p(num - 1))
+  # REGRESSIVE CASE: add p for next integer in sequence
+  # forumula: p(num) = p(p(num - 1)) + p(num - p(num - 1))
   building_num = sequence.length + 1
   # if num is 4 previous_p is at index 2 (value: 2)
   previous_p = sequence[building_num - 2]

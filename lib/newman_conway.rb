18401889b944a70b57ec6cfea8bcc6ee3568ca8a
Julia A Kingrey <juliakingreyworks@gmail.com>
2020-03-20 18:08:35 -0700

    Passing all newman_conway tests which is very exciting because this is the first time I've coded a regressive function without ANY outside help.

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -10,29 +10,26 @@ def newman_conway(num)
   elsif num == 2
     return "1 1"
   else
-    p_helper(num)
+    p_helper(num, [1, 1])
   end 
 end
 
-def p_helper(num)
-  sequence = [1, 1]
-
-  # when do we memo???? I guess we only need to keep track of previous_p and p_previous_p values
-
-  # p(num) = p(p(num - 1)) + p(num - p(num - 1))
+def p_helper(num, sequence)
 
   # need to find p of all whole integers preceding num and add them to sequence
-  # code below seems to work if I provide n and sequence for integers preceding n
-  build = num - 2
-  build.times do
-    # if num is 4 previous_p is at index 2 (value: 2)
-    previous_p = sequence[num - 2]
-    # now we need to look up p of the previous_p value
-    p_previous_p = sequence[previous_p - 1]
-    # find p of number minus previous number's p, then add it to p_previous_p
-    p = p_previous_p + sequence[(num - previous_p) - 1]
-    sequence << p
-  end
 
-  return sequence
+  # BASE CASE: stop when count of numbers in sequence array is the same as `num`
+  return sequence.join(" ") if num == sequence.length
+
+  # REGRESSIVE CASE: 
+  # forumula is p(num) = p(p(num - 1)) + p(num - p(num - 1))
+  building_num = sequence.length + 1
+  # if num is 4 previous_p is at index 2 (value: 2)
+  previous_p = sequence[building_num - 2]
+  # now we need to look up p of the previous_p value
+  p_previous_p = sequence[previous_p - 1]
+  # find p of number minus previous number's p, then add it to p_previous_p
+  p = p_previous_p + sequence[(building_num - previous_p) - 1]
+  sequence << p
+  return p_helper(num, sequence)
 end

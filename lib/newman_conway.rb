be0ae7eca532904397aedd8c1cdb3e585ec4c267
Emily Ball <emily.ball@nordstrom.com>
2020-03-22 13:14:32 -0700

    passing newman_conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,28 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError.new("Invalid input, must be greater than 0") if num < 1
+  
+  list = []
+  i = 0
+  until i == num || i == 2
+    list << 1
+    i += 1
+  end
+  
+  return list.join(" ") if num < 3
+
+  return nc_helper(num, list).join(" ")
+end
+
+def nc_helper(n, list)
+  return 1 if n < 3
+  
+  if list[n-2]
+    n_minus_one = list[n-2]
+  else
+    n_minus_one = (nc_helper(n-1, list)).last
+  end
+
+  return list << list[n_minus_one - 1] + list[n - n_minus_one - 1]
 end

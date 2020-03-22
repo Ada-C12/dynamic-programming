b0becce07fba6e27cdeb08bc5fe973d6b04fa091
Emily Ball <emily.ball@nordstrom.com>
2020-03-22 13:49:30 -0700

    passing max_subarray

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,19 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
-    return 0 if nums == nil
+    return nil if nums.nil? || nums.empty?
     
-    raise NotImplementedError, "Method not implemented yet!"
+    max_so_far = nums[0]
+    max_ending_here = 0
+
+    nums.each do |num|
+        max_ending_here = max_ending_here + num
+        
+        max_ending_here = num if max_ending_here < num
+        
+        max_so_far = max_ending_here if max_so_far < max_ending_here
+    end
+    
+    return max_so_far
 end

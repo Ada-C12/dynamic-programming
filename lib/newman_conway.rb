933d72dc6a5a4c5184c575d698201468e8f73454
Angele Zamarron <angelez@allenai.org>
2020-03-26 18:58:04 -0700

    space-time complexity for newman-conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,7 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: o(n) - runtime for most values of num and in worst case is n - 2 calculations
+# Space Complexity: o(n) - size of sequence and answer string has linear dependency on value of num
 def newman_conway(num)
   raise ArgumentError if num == 0
   return "1" if num == 1
@@ -9,9 +9,12 @@ def newman_conway(num)
 
   sequence = [0, 1, 1]
 
+  answer = "1 1"
+
   (3..num).each do |n|
     p_last = sequence[n - 1]
     sequence << (sequence[p_last] + sequence[n - p_last])
+    answer += " #{sequence[n]}"
   end
-  return sequence[1..-1].join(' ')
+  return answer
 end

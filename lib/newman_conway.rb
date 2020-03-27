63827829a25811f9c93331bd0fcd268bd0781e91
Angele Zamarron <angelez@allenai.org>
2020-03-26 18:51:11 -0700

    passing newman-conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,15 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num == 0
+  return "1" if num == 1
+  return "1 1" if num == 2
+
+  sequence = [0, 1, 1]
+
+  (3..num).each do |n|
+    p_last = sequence[n - 1]
+    sequence << (sequence[p_last] + sequence[n - p_last])
+  end
+  return sequence[1..-1].join(' ')
 end

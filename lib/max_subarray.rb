7f6f98fdf946aab3d8e0ef3f14804bbeeda093fa
Hallie <idhallie@gmail.com>
2020-03-28 13:57:31 -0700

    Passes all Newman-Conway tests.

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -2,7 +2,21 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def max_sub_array(nums)
-    return 0 if nums == nil
+    return if nums == nil || nums.length == 0
     
-    raise NotImplementedError, "Method not implemented yet!"
+    max_so_far = 0
+    max_ending_here = 0
+    
+    nums.each do |num|
+        max_ending_here += num
+        if max_ending_here < 0
+            max_ending_here = 0
+        end
+        
+        if max_so_far < max_ending_here
+            max_so_far = max_ending_here
+        end
+    end
+    
+    return max_so_far
 end

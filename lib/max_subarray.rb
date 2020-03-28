e2e47d20eaf9aa5122de47c06cab58d471c842e9
Hallie <idhallie@gmail.com>
2020-03-28 14:42:10 -0700

    Passes max subarray tests.

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,22 +1,21 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n); it iterates over each element in the nums array once.
+# I don't think the max method adds meaningful time complexity. ?
+
+# Space Complexity: O(1); makes the same number of variables regardless of nums size.
+# Variables hold a single value and do not scale.
 def max_sub_array(nums)
-    return if nums == nil || nums.length == 0
-    
-    max_so_far = 0
-    max_ending_here = 0
-    
-    nums.each do |num|
-        max_ending_here += num
-        if max_ending_here < 0
-            max_ending_here = 0
-        end
-        
-        if max_so_far < max_ending_here
-            max_so_far = max_ending_here
-        end
-    end
-    
-    return max_so_far
+  return if nums == nil || nums.length == 0
+  
+  max_so_far = nums[0]
+  max_ending_here = nums[0]
+  i = 1
+  
+  while i < nums.length
+    max_ending_here = [nums[i], max_ending_here + nums[i]].max
+    max_so_far = [max_so_far, max_ending_here].max
+    i += 1
+  end
+  
+  return max_so_far
 end

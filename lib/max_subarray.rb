548872eafd91b87d3b2a45d2d5f62b17a4dc3248
dora1405 <li.dora05@gmail.com>
2020-03-28 21:33:59 -0700

    passed tests for max_subarray

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,29 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
     return 0 if nums == nil
+    return nums[0] if nums.length == 1
+
+    max = nums[0]
+    current_sum = nums[0]
+
+    i = 1
+
+    while i < nums.length
+      if nums[i] > current_sum + nums[i]
+        current_sum = nums[i]
+      else
+        current_sum += nums[i]
+      end
+      
+      if max < current_sum
+        max = current_sum
+      end
+
+      i += 1
+    end
+
+    return max
     
-    raise NotImplementedError, "Method not implemented yet!"
 end
--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,25 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num < 1
+  
+  return num.to_s if num == 1
+
+  answer = []
+
+  one_previous = 1
+  two_previous = 1
+  n = 2
+  
+  while n < num
+    temp = one_previous + two_previous
+    answer << temp
+    two_previous = one_previous
+    one_previous = temp
+    n += 1
+  end
+
+  return answer.join(" ")
+
+
 end

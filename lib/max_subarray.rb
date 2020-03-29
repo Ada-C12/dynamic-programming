ad589ad2794fcd6f7aeb7603f98a4c79261a5bd3
Janice Huang <janicehuang@gmail.com>
2020-03-28 20:54:53 -0700

    get max sub array tests to pass

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -2,7 +2,18 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+	return 0 if nums == nil
+	return nil if nums == []
+	return nums[0] if nums.length == 1
+	local_max = 0
+	global_max = -Float::INFINITY
+	index = 0
+	while index < nums.length
+		local_max = [nums[index], local_max + nums[index]].max
+		if local_max > global_max
+			global_max = local_max
+		end
+		index += 1
+	end
+	return global_max
 end

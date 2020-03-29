7002290dbe1c77c99ac8904a7c086f48ea0b057f
Sara Shah Baig <sarashahbaig@gmail.com>
2020-03-29 13:33:09 -0700

    completed

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,18 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: o(n)
+# Space Complexity: o(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  # raise NotImplementedError, "newman_conway isn't implemented"
+  return "1" if num == 1
+  array = [1, 1]
+  i = 2
+
+  until i == num
+    previous = array[i - 1]
+    current = array[previous - 1] + array [i - previous]
+    array << current
+    i += 1
+  end
+  return array.join("")
 end

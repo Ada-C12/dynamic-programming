6999b30b2992450345d94af7d82e89361ba486c3
Julia Bouvier <juliabouv@gmail.com>
2020-03-29 14:40:30 -0700

    Implemented newman_conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,21 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num < 1
+  
+  storage = []
+
+  i = 1
+  while i <= num
+    if i < 3
+      storage[i] = 1
+    else
+      storage[i] = storage[storage[i - 1]] + storage[i - storage[i - 1]]
+    end
+    i += 1
+  end
+
+  return storage.join(" ")[1..-1] # fencepost slice
 end

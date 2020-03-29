81a0d62f284acbee777cc3c53db02cd265779fc3
Sara Shah Baig <sarashahbaig@gmail.com>
2020-03-29 13:32:54 -0700

    completed and passing the test

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,22 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: o(n)
+# Space Complexity: o(1)
 def max_sub_array(nums)
+    # raise NotImplementedError, "Method not implemented yet!"
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+    return nil if nums == []
+    array_max = 0
+    current_max = 0
+
+    nums.each do |num|
+        array_max = array_max + num
+        array_max = array_max < 0 ? 0 : array_max
+        if array_max > current_max
+            current_max = array_max
+        end
+    end
+    if current_max == 0
+        current_max = nums.max
+    end
+    return current_max
 end

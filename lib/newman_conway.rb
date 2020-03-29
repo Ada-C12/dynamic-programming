e8b061eefc61054a2b57b2bd267ce02ca0a21285
Yasmin <yasmin312002@hotmail.com>
2020-03-29 13:47:05 -0700

    implemented newman-conway method, passing tests

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,23 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: o(n)
+# Space Complexity: o(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  # raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num < 1
+  return "1" if num == 1
+  return "1 1" if num == 2
+
+  newman_conway = [0, 1, 1]
+  output = "1 1"
+
+  i = 3
+
+  while i <= num 
+    #p(i) = p(p(i-1)) + p(i - p(i - 1))
+    newman_conway[i] = newman_conway[newman_conway[i - 1]] + newman_conway [i - newman_conway[i - 1]]
+    output += " #{newman_conway[i]}"
+    i += 1
+  end
+    return output
 end

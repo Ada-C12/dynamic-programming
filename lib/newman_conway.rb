425cd8b54f5f88fc6cbf1a82aceac62a64bbaf06
Ga-Young Jin <gyjinn@gmail.com>
2020-03-28 23:56:21 -0700

    completed newman conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,23 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n * num) ?
+# Space Complexity: O(num) ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  newman_conway_helper([0, 1, 1], 3, num)
+end
+
+def newman_conway_helper(p, current, n)
+  if n <= 0
+    raise ArgumentError, "Input must be greater than 0."
+  end
+  
+  return "1" if n == 1
+  return "1 1" if n == 2
+  
+  if current > n 
+    p.shift # S: O(n)?  T: O(n)?
+    return p.join(" ")
+  end
+  
+  p << p[p[current - 1]] + p[current - p[current - 1]]
+  
+  return newman_conway_helper(p, current + 1, n) # T: O(num) calls this recursively about num number of times
 end

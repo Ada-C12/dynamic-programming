a61191d8abcc290f6ef4ee0256709620b933f17d
Eve Le <nguyenthao.lee@gmail.com>
2020-03-30 01:40:24 -0700

    implemented newman_conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,15 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num < 1
+  output = Array.new()
+  num.times do |i|
+    if i < 2
+      output << 1
+    else
+      previous = output[i - 1]
+      output << output[previous - 1] + output[i - previous]
+    end
+  end
+  return output.join(" ")
 end

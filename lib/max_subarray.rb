2c397530abb6e9e0b7daee21b922167ea4f78757
Michaela Morrow <morrow.michaela@gmail.com>
2020-03-30 12:35:49 -0400

    Finished exercises

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,30 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) where n is the length of nums
+# Space Complexity: O(1) because you only keep track of 2 numbers
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+  return 0 if nums == nil
+  return nil if nums.empty?
+  
+  max_by_here = nums[0]
+  max_so_far = nums[0]
+  
+  nums.each_with_index do |num, index|
+    unless index == 0
+      if max_so_far < 0
+        if num > max_so_far
+          max_so_far = num
+        end
+      else
+        max_by_here += num
+        
+        if max_by_here < 0
+          max_by_here = 0
+        elsif max_by_here > max_so_far
+          max_so_far = max_by_here
+        end
+      end
+    end
+  end
+  
+  return max_so_far
 end
--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,30 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n) where n is num
+# Space Complexity: O(n) where n is num
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  
+  if num == 0
+    raise ArgumentError.new("You cannot enter 0")
+  elsif num == 1
+    return "1"
+  end
+  
+  result = [1, 1]
+  
+  n = 3
+  
+  until result.length >= num
+    inner_first_num = result[-1]
+    first_num = result[(inner_first_num - 1)]
+    
+    
+    inner_second_num = n - inner_first_num
+    second_num = result[(inner_second_num - 1)]
+    new_addition = first_num + second_num
+    result << new_addition
+    n += 1
+  end
+  
+  return result.join(" ")
 end

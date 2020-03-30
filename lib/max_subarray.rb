ba207e2e067f596337faa284108c1fb4bece6b4d
Eve Le <nguyenthao.lee@gmail.com>
2020-03-30 01:58:01 -0700

    Implemented max_sub_array

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,21 @@
-
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+  result = nil
+  if nums == nil
+    result = 0
+  elsif nums.length > 0
+    max_so_far = 0
+    max_ending_here = 0
+    largest_element = nums[0]
+    nums.each do |element|
+      max_ending_here = max_ending_here + element
+      
+      max_ending_here = 0 if max_ending_here < 0
+      max_so_far = max_ending_here if max_so_far < max_ending_here        
+      largest_element = element if element > largest_element
+    end
+    result = (largest_element < 0) ? largest_element : max_so_far
+  end
+  return result
 end

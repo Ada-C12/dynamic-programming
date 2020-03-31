41dadb58c3c5fb33bce2ef66ab7b4734a7ca3cf1
C Gutierrez <gutierrez.clara.es@gmail.com>
2020-03-31 01:20:03 -0700

    max_subarray basic functionality

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,34 @@
 
 # Time Complexity: ?
 # Space Complexity: ?
-def max_sub_array(nums)
-    return 0 if nums == nil
+def max_sub_array(number_array)
+    length = number_array.length
+    if length == 0 
+        return nil
+    elsif length == 1
+        return number_array[0]
+    end
     
-    raise NotImplementedError, "Method not implemented yet!"
+    max_so_far = number_array[0]
+    max_ending_here = 0
+    
+    i = 0
+    
+    until i == length do
+        
+        max_ending_here = max_ending_here + number_array[i]
+        
+        if max_so_far < max_ending_here
+            max_so_far = max_ending_here
+            i += 1
+        elsif max_ending_here < 0
+            max_ending_here = 0
+            i += 1
+        else
+            i+= 1
+        end
+    end
+    
+    return max_so_far
+    
 end

70c8e21c92b2026cb3cb6c1e5684c3d4f6a90405
geomsb <cpgeorgina.sanchez@gmail.com>
2020-03-31 18:20:41 -0700

    dynamic programming

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,21 @@
-
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
-    return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+  return 0 if nums == nil
+
+  max_so_far = 0
+  max_ending_here = 0
+
+  i = 0
+  while i < nums.length
+    max_ending_here = max_ending_here + a[i]
+    if(max_ending_here < 0)
+      max_ending_here = 0
+    elsif(max_so_far < max_ending_here)
+      max_so_far = max_ending_here
+    end
+    i+=1
+  end
+
+  return max_so_far
 end

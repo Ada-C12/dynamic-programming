e0f503c91479f4038cc35acc4694c4db6667b34f
morganschuler <morganlschuler@gmail.com>
2020-03-31 18:49:34 -0700

    Problems done

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,24 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  if num == 0
+    throw ArgumentError, "0 is not valid"
+  end
+  
+  seq = []
+  
+  seq[1] = 1
+  seq[2] = 1
+  
+  if num == 1 || num == 2
+    return seq[1..num].join(' ')
+  end
+  
+  n = 3
+  while n <= num
+    seq[n] = seq[seq[n - 1]] + seq[n- seq[n-1]]
+    n += 1
+  end
+  
+  return seq[1..num].join(' ')
 end

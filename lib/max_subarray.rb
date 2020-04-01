999d89126803351b954d0463a61a2882a6827b38
geomsb <cpgeorgina.sanchez@gmail.com>
2020-03-31 22:28:35 -0700

    max subarray fixed

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -3,12 +3,32 @@
 def max_sub_array(nums)
   return 0 if nums == nil
 
+  j = 0
+  k = 0
+  while j < nums.length
+    if nums[j] < 0
+      k += 1
+    end
+    j += 1
+  end
+  
+  if k == nums.length
+    return nums.max
+  else 
+    return kadane(nums)
+  end
+end 
+
+
+def kadane(nums)
+  return 0 if nums == nil
+
   max_so_far = 0
   max_ending_here = 0
 
   i = 0
   while i < nums.length
-    max_ending_here = max_ending_here + a[i]
+    max_ending_here = max_ending_here + nums[i]
     if(max_ending_here < 0)
       max_ending_here = 0
     elsif(max_so_far < max_ending_here)

4fd29c2809eab82abae3c0cc2383e330525ae747
Sam <samcoll@Samanthas-MBP.localdomain>
2020-04-04 12:06:29 -0700

    newman_conway all tests pass

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,22 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
-end
+  raise ArgumentError if num < 1
+  return "1" if num == 1
+  
+  seq = [0, 1, 1]
+  i = 3
+  result = "1 1"
+  
+  until i >= num + 1
+    current = seq[seq[i-1]] + seq[i - (seq[i-1])]
+    seq << current
+    result += " #{current}"
+    i += 1
+  end
+  
+  return result
+end 
+
+
+#pp newman_conway(13)
